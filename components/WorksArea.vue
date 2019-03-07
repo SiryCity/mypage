c7954a1ed36d4d333de74f7c4f47bcd8dee5f9fe
rm <sirycity2018@gmail.com>
2019-03-07 14:31:44 +0900

    リンク追加

--- components/WorksArea.vue
+++ components/WorksArea.vue
@@ -1,17 +1,23 @@
 <template lang="pug">
-  div.works
+  a.works(:href='works.url')
     img(:src='works.src')
     div.works__bottom
       h2 {{works.title}}
       h3 {{works.org}}
       div {{works.text}}
+      img.works__static(:src='link')
 </template>
 
 <script>
+import link from '~/assets/link.svg'
 export default {
   props:['works'],
+  computed: {
+    link: () => link,
+  } 
 }
 </script>
+
 
 <style lang="stylus" scoped>
 .works
@@ -22,21 +28,32 @@ export default {
   overflow hidden
   border-radius 10px
   box-shadow 0 3px 6px #ccc
+  text-decoration none
+  color #333
 .works > img
   width 100%
   height 203px
-  object-fit contain
-  background-color gray
+  object-fit cover
+  background-color #ccc
 .works__bottom
   width 96%
   height 125px
   margin 0 2%
+  position relative
 
 .works__bottom > h2
-  font-size 20px
-  margin -16px 0 0 0
+  font-size 22px
+  margin -18px 0 0 0
 .works__bottom > h3
   font-size 16px
-  margin 2px 0
+  margin 0 0 2px 0
+
+.works__static
+  width 30px
+  height 30px
+  position absolute
+  right 4px
+  top 24px
+
 </style>
 
--- store/works.js
+++ store/works.js
@@ -1,17 +1,17 @@
-import link from '~/assets/link.svg'
+import shibata279 from '~/assets/works/shibata279.jpg'
 
 export const getters = {
   works: () =>
     [
       {
-        src: link,
+        src: shibata279,
         title: '柴田アクティブストリート',
         url: 'https://shibataactivestreet.com/',
         org: '名古屋市南区',
         text: '簡単な解説簡単な解説簡単な解説簡単な解説簡単な解説簡単な解説簡単な解説簡単な解説簡単な解説簡単な解説簡単な解説',
       },
       {
-        src: link,
+        src: shibata279,
         title: '柴田アクティブストリート',
         url: 'https://shibataactivestreet.com/',
         org: '名古屋市南区',
