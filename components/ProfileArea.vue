6543ac7a5e6ce4be617e317ce9cce982ec2a4ded
rm <sirycity2018@gmail.com>
2019-03-15 18:51:43 +0900

    homeに追加

--- components/ProfileArea.vue
+++ components/ProfileArea.vue
@@ -14,45 +14,51 @@
   justify-content center
   border-radius 10px
   box-shadow 0 3px 6px #ccc
-
-.profile-area > h3
-  margin 4px 0 0px
-  padding 0
-  font-size 16px
-
-.profile-area > h1
-  margin 16px 0 -4px
-  padding 0
-  font-size 42px
-  font-weight normal
-
-.profile-area > h1:first-letter
-  color #0073c2
-
-.profile-area > h2
-  margin -4px 0 22px
-  padding 0
-  font-size 16px
   
-.profile-area > p
-  display block
-  word-wrap break-word
-  padding 0
-  margin 0
+  h1
+    margin 16px 0 -4px
+    padding 0
+    font-size 42px
+    font-weight normal
 
-.profile-area > div
-  display flex
-  justify-content flex-start
-  height 28px
-  margin 16px 0 0
+    &:first-letter
+      color #0073c2
 
-.profile-area > div > a
-  width 28px
-  height 28px
-  margin 0 4px 0 0
+  h2
+    margin -4px 0 22px
+    padding 0
+    font-size 16px
 
-.profile-area > div > a > img
-  width 28px
-  height 28px
+  h3
+    margin 4px 0 0px
+    padding 0
+    font-size 16px
+
+  h4
+    margin 0 0 22px
+    padding 0
+    font-size 20px
+
+  p
+    display block
+    word-wrap break-word
+    padding 0
+    margin 0
+    line-height 170%
+
+  div
+    display flex
+    justify-content flex-start
+    height 28px
+    margin 16px 0 0
+
+    a
+      width 28px
+      height 28px
+      margin 0 4px 0 0
+
+      img
+        width 28px
+        height 28px
 </style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -5,20 +5,31 @@
       h3 Web Engineer & Web Designer
       h1 大同太郎
       h2 DAIDOOOO Taroo
-      p 2017-19年, 大同大学 情報学研究科にて情報学(JavaScript, Python)を専攻.
-      p 2019年, 在学中に名古屋市を拠点としてフリーランスエンジニアの活動を開始.
-      p 2019年3月現在, 元気にやってます :)
-    icon-area(:icon='icon')
+      p 1994.11.15 (24)
+      p.
+        2017-19年, 大同大学 大学院 情報学研究科にてWebエンジニアリングを専攻. 
+        2019年, 在学中にフリーランスエンジニアとして活動を開始. 
+    icon-area(:icon='photo')
     profile-area
-      h2 JavaScriptで世界がhappy<3<3<3
-      p JavaScriptを用いたWebアプリケーションやPRサイト,
-      p JavaScript + Pythonを用いた業務効率化システムの開発に加え,
-      p 活動で得た知見を各所で発信しています.
-      p 宜しければご覧ください :)▼
+      h4 I ♥ JavaScript
+      p.
+        参画したプロジェクトでは主にJavasctipt, 時々Pythonを用いて開発を行っています. 
+        WebアプリケーションやPRサイトの制作, 業務効率化システムの開発など... 
+        活動で得た知見は各所で発信しています.
       div.link-area
         a(href='#'): img(:src='twitter')
         a(href='https://github.com/SiryCity'): img(:src='github')
         a(href='#'): img(:src='link')
+    icon-area(:icon='nagoya')
+    profile-area
+      h4 I ♥ Nagoya
+      p.
+        Web不毛の地名古屋を拠点に今日も奮闘しています. 
+        本格的に活動を初めてからは, 中小規模の団体や学生様からよくお話を頂きます. 
+        Webコンテンツに興味はあるけどお金が... なんて方がいたら, 
+      nuxt-link(to='/contact') お話を聞かせてください :)
+    
+        
 </template>
 
 <script>
@@ -26,9 +37,11 @@ import IconArea from '~/components/IconArea.vue'
 import ProfileArea from '~/components/ProfileArea.vue'
 
 import icon from '~/assets/icon.svg'
-import link from '~/assets/link.svg'
-import twitter from '~/assets/twitter.svg'
-import github from '~/assets/github.svg'
+import photo from '~/assets/photo.svg'
+  import link from '~/assets/link.svg'
+  import twitter from '~/assets/twitter.svg'
+  import github from '~/assets/github.svg'
+import nagoya from '~/assets/nagoya.svg'
 
 export default {
   components: {
@@ -37,9 +50,11 @@ export default {
   },
   computed: {
     icon: () => icon,
-    link: () => link,
-    twitter: () => twitter,
-    github: () => github,
+    photo: () => photo,
+      link: () => link,
+      twitter: () => twitter,
+      github: () => github,
+    nagoya: () => nagoya
   } 
 }
 </script>
