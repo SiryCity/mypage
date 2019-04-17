8a4fae3e18b26b3140267f8d40548bdfbef43d08
rm <sirycity2018@gmail.com>
2019-04-18 00:45:06 +0900

    プロフィール更新

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   div.main
     icon-area(:icon='icon')
     profile-area
-      h3 Web Engineer & Web Designer
+      h3 名古屋発Webエンジニア
       h1 水谷竜斗
       h2 MIZUTANI Ryuto
-      p 1994.11.15 (24)
+      p 1994年、名古屋に誕生。
       p.
         2017-19年、大同大学 大学院 情報学研究科にてWebエンジニアリングを専攻。
         2019年、在学中にエンジニアとして個人活動を開始。
@@ -13,7 +13,7 @@
     profile-area
       h4 I ♥ JavaScript
       p.
-        参画したプロジェクトでは主にJavasctipt、時々Pythonを用いて開発を行っています。
+        主としてJavaScript、時々PythonやPHPを用いて開発を行っています。
         WebアプリケーションやPRサイトの制作、業務効率化システムの開発など... 
         活動で得た知見は各所で発信しています。
       div.link-area
@@ -24,18 +24,18 @@
     profile-area
       h4 I ♥ challenging
       p.
-        ご依頼の相手は中小規模の団体や学生が殆ど。
-        どなたも斬新なWebコンテンツを考案し、精力的に活動されています。
-        少ない予算で大きな夢をお持ちの方、
-      nuxt-link.link-box(to='/contact') ご連絡お待ちしております :)
+        メンバーは中小規模の団体や学生が殆ど。
+        当たって砕けろの精神で最新技術を積極的に導入しています!
+        最新のWeb技術で面白いコンテンツを作りたい、なんて考えていませんか?ぜひとも
+      nuxt-link.link-box(to='/contact') お話を聞かせてください :)
     icon-area(:icon='nagoya')
     profile-area
       h4 I ♥ Nagoya
       p.
-        私自身が生まれ育った名古屋を拠点に活動しています。 
+        生まれ育った名古屋を拠点に活動しています。 
         名古屋は重工業の街ですが、だからこそITの可能性が溢れていると感じます。
         RPA(業務効率化)に関するWebシステムをお考えの方も、
-      nuxt-link.link-box(to='/contact') お話を聞かせてください :)
+      nuxt-link.link-box(to='/contact') ご連絡お待ちしております :)
     
         
 </template>
@@ -56,7 +56,7 @@ export default {
   head: () => 
     ({
       titleTemplate: null,
-      title: '水谷竜斗 | Webエンジニア',
+      title: '水谷竜斗 | 名古屋発Webエンジニア',
     }),
   components: {
     IconArea,
