9326eacd4b6dc0eb9909c806b1d089999ed1cde3
rm <sirycity2018@gmail.com>
2019-04-26 00:18:37 +0900

    fix description

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,12 +27,12 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '水谷竜斗(MIZUTANI Ryuto) IT不毛の地名古屋から世界に羽ばたく(予定)Webエンジニアです。' },
+      { hid: 'description', name: 'description', content: '水谷竜斗(MIZUTANI Ryuto) | 名古屋発世界に羽ばたくWebエンジニア。' },
       { hid: 'og:site_name', property: 'og:site_name', content: '水谷竜斗 | 名古屋発Webエンジニア' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: 'https://rm-js.com' },
       { hid: 'og:title', property: 'og:title', content: '水谷竜斗 | 名古屋発Webエンジニア' },
-      { hid: 'og:description', property: 'og:description', content: '水谷竜斗(MIZUTANI Ryuto) IT不毛の地名古屋から世界に羽ばたく(予定)Webエンジニアです。' },
+      { hid: 'og:description', property: 'og:description', content: '水谷竜斗(MIZUTANI Ryuto) | 名古屋発世界に羽ばたくWebエンジニア。' },
       { hid: 'og:image', property: 'og:image', content: '/icon.svg'}
     ],
     link: [
